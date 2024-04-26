4741839aec3f545cc1a915c1c7418cd3b631da43
zicy <kim@zicy.dk>
2024-04-27 01:24:15 +0200

    Use localstorage for the csv file, during the session instead of fetching it every time

--- js/script.js
+++ js/script.js
@@ -1,80 +1,94 @@
-let xhr; // Define xhr outside the function to make it accessible globally
+
+
+localStorage.removeItem('csvData');
 
 function searchCSV(inputValue) {
 
-    // Cancel previous XMLHttpRequest if it exists
-    if (xhr && xhr.readyState !== 4) {
-        xhr.abort();
+    const csvDataFile = "toeffe_lager.csv?v=15"
+
+    // Check if the CSV data is already stored in localStorage
+    const storedCSVData = localStorage.getItem('csvData');
+    if (storedCSVData) {
+        processData(storedCSVData);
+    } else {
+        const csvFile = csvDataFile; // Path to your CSV file
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', csvFile, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                const csvData = xhr.responseText;
+                localStorage.setItem('csvData', csvData); // Store CSV data in localStorage
+                processData(csvData);
+            }
+        };
+        xhr.send();
     }
 
-    const csvFile = 'toeffe_lager.csv?v=14'; // Path to your CSV file
-    xhr = new XMLHttpRequest();
-    xhr.open('GET', csvFile, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            const csvData = xhr.responseText;
-            const rows = csvData.split('\n');
-            let random_id = 0;
-            let result = '';
-            let count = 0; // Counter for limiting results
-            const searchResultsContainer = document.getElementById('search-results');
-            searchResultsContainer.innerHTML = ''; // Clear previous results
-            for (let i = 0; i < rows.length; i++) {
-                const columns = rows[i].split(',');
-                if (columns.length >= 2 && columns[1].toLowerCase().includes(inputValue.toLowerCase())) {
-                    const newDiv = document.createElement('div');
-                    let random_id = Math.floor(Math.random() * 1000000)
-
-                    const item_name = columns[1];
-                    const lunch_box = columns[2];
-                    const lunch_box_row = columns[3];
-                    const lunch_box_position = columns[4];
-                    const image_name = columns[5];
-
-                    // Get lunch box information
-                    const lunch_box_letter = columns[2].match(/[^\d]+/)[0];
-                    const lunch_box_number = parseInt(columns[2].match(/\d+/)[0]);
-
-                    var formatted_name = "";
-                    if (image_name.length <= 1) {
-                        formatted_name = item_name.toLowerCase().replace(/ /g, '_');
-                    } else {
-                        formatted_name = image_name;
-                    }
-
-                    newDiv.innerHTML = `<img class="color-${lunch_box_letter}" src="images/block_icons/${formatted_name}.png" onerror="this.onerror=null; this.src='images/MissingTextureBlock.png'" ><div class="result-name">${item_name} </div><div class="result-extra-info"><div>Lunchbox <span class="">${lunch_box}</span></div> <div>Row <span class="">${lunch_box_row}</span></div> <div>Position <span class="">${lunch_box_position}</span></div></div>`;
-                    newDiv.classList.add('result', 'pop-in'); // Add classes 'result' and 'pop-in' to each div
-                    newDiv.style.animationDelay = `${count * 0.1}s`; // Adjust delay time as needed
-                    searchResultsContainer.appendChild(newDiv); // Append new result div
-                    // Add marker to map
-                    addMarker(item_name, lunch_box_letter, lunch_box_number, lunch_box_row, lunch_box_position, random_id); // Pass latitude and longitude
-
-                    // Add event listeners to the dynamically created div
-                    newDiv.addEventListener('mouseenter', function () {
-                        hideOtherMarkers(random_id); // Call function to hide other markers
-                    });
-                    newDiv.addEventListener('mouseleave', function () {
-                        showOtherMarkers(); // Clear marker when mouse leaves the result element
-                    });
-                    newDiv.addEventListener('click', function () {
-                        saveMarker(lunch_box_letter + lunch_box_number, random_id, item_name); // Clear marker when mouse leaves the result element
-                    });
-
-                    count++; // Increment counter
-                }
-            }
-            if (count === 0) {
+    function processData(csvData) {
+        // Rest of your code to process CSV data...
+        // This part remains unchanged from your original code
+        // You can use the csvData variable here to process the CSV data
+        const rows = csvData.split('\n');
+        let random_id = 0;
+        let result = '';
+        let count = 0; // Counter for limiting results
+        const searchResultsContainer = document.getElementById('search-results');
+        searchResultsContainer.innerHTML = ''; // Clear previous results
+        for (let i = 0; i < rows.length; i++) {
+            const columns = rows[i].split(',');
+            if (columns.length >= 2 && columns[1].toLowerCase().includes(inputValue.toLowerCase())) {
                 const newDiv = document.createElement('div');
+                random_id = Math.floor(Math.random() * 1000000)
 
-                newDiv.innerHTML = `<img class="" src="images/MissingTextureBlock.png" ><div class="result-name">Not found: ${inputValue} </div><div class="result-extra-info"><div>Lunchbox <span class="">n/a</span></div> <div>Row <span class="">n/a</span></div> <div>Position <span class="">n/a</span></div></div>`;
+                const item_name = columns[1];
+                const lunch_box = columns[2];
+                const lunch_box_row = columns[3];
+                const lunch_box_position = columns[4];
+                const image_name = columns[5];
+
+                // Get lunch box information
+                const lunch_box_letter = columns[2].match(/[^\d]+/)[0];
+                const lunch_box_number = parseInt(columns[2].match(/\d+/)[0]);
+
+                var formatted_name = "";
+                if (image_name.length <= 1) {
+                    formatted_name = item_name.toLowerCase().replace(/ /g, '_');
+                } else {
+                    formatted_name = image_name;
+                }
+
+                newDiv.innerHTML = `<img class="color-${lunch_box_letter}" src="images/block_icons/${formatted_name}.png" onerror="this.onerror=null; this.src='images/MissingTextureBlock.png'" ><div class="result-name">${item_name} </div><div class="result-extra-info"><div>Lunchbox <span class="">${lunch_box}</span></div> <div>Row <span class="">${lunch_box_row}</span></div> <div>Position <span class="">${lunch_box_position}</span></div></div>`;
                 newDiv.classList.add('result', 'pop-in'); // Add classes 'result' and 'pop-in' to each div
                 newDiv.style.animationDelay = `${count * 0.1}s`; // Adjust delay time as needed
                 searchResultsContainer.appendChild(newDiv); // Append new result div
+                // Add marker to map
+                addMarker(item_name, lunch_box_letter, lunch_box_number, lunch_box_row, lunch_box_position, random_id); // Pass latitude and longitude
+
+                // Add event listeners to the dynamically created div
+                newDiv.addEventListener('mouseenter', function () {
+                    hideOtherMarkers(random_id); // Call function to hide other markers
+                });
+                newDiv.addEventListener('mouseleave', function () {
+                    showOtherMarkers(); // Clear marker when mouse leaves the result element
+                });
+                newDiv.addEventListener('click', function () {
+                    saveMarker(lunch_box_letter + lunch_box_number, random_id, item_name); // Clear marker when mouse leaves the result element
+                });
+
+                count++; // Increment counter
             }
         }
-    };
-    xhr.send();
+        if (count === 0) {
+            const newDiv = document.createElement('div');
+
+            newDiv.innerHTML = `<img class="" src="images/MissingTextureBlock.png" ><div class="result-name">Not found: ${inputValue} </div><div class="result-extra-info"><div>Lunchbox <span class="">n/a</span></div> <div>Row <span class="">n/a</span></div> <div>Position <span class="">n/a</span></div></div>`;
+            newDiv.classList.add('result', 'pop-in'); // Add classes 'result' and 'pop-in' to each div
+            newDiv.style.animationDelay = `${count * 0.1}s`; // Adjust delay time as needed
+            searchResultsContainer.appendChild(newDiv); // Append new result div
+        }
+    }
 }
+
 
 function hideOtherMarkers(random_id) {
     const mapContainer = document.getElementById('map-pins');
@@ -117,7 +131,7 @@ function saveMarker(lunch_box, marker_id, marker_name) {
     var copiedElems = [];
 
     // Loop through each suffix to get original elements and clone them
-    suffixes.forEach(function(suffix) {
+    suffixes.forEach(function (suffix) {
         var originalElem = document.getElementById(marker_id + suffix);
         if (originalElem) {
             originalElems.push(originalElem);
@@ -128,7 +142,7 @@ function saveMarker(lunch_box, marker_id, marker_name) {
             } else if (suffix === '_pulse') {
                 clonedElem.classList.add('pulse-saved');
             }
-            
+
             clonedElem.classList.add('pin-pointer');
             clonedElem.addEventListener('click', function () {
                 removeSavedMarker(marker_id); // Clear marker when mouse leaves the result element
@@ -143,7 +157,7 @@ function saveMarker(lunch_box, marker_id, marker_name) {
         var copiedParent = originalParent.cloneNode(false); // Shallow copy without children
 
         // Append the copied child divs to the copied parent div
-        copiedElems.forEach(function(copiedElem) {
+        copiedElems.forEach(function (copiedElem) {
             copiedParent.appendChild(copiedElem);
         });
 
@@ -157,13 +171,13 @@ function saveMarker(lunch_box, marker_id, marker_name) {
         // Create a new paragraph element
         var paragraph = document.createElement('p');
         paragraph.id = marker_id + '_saved-list';
-        
+
         // Set the inner HTML of the paragraph
         paragraph.innerHTML = marker_name + '<span>×</span>';
-        
+
         // Get the div with the ID "map-pins-saved-list"
         var savedListDiv = document.getElementById('map-pins-saved-list');
-        
+
         // Check if the div exists
         if (savedListDiv) {
             // Append the paragraph to the div
@@ -184,7 +198,7 @@ function removeSavedMarker(marker_id) {
     // Array of suffixes for the elements to clone
     var suffixes = ['_marker', '_pulse', '_text', '_saved-list'];
 
-    suffixes.forEach(function(suffix) {
+    suffixes.forEach(function (suffix) {
         var originalElem = document.getElementById(marker_id + suffix);
         if (originalElem) {
             originalElem.remove();
