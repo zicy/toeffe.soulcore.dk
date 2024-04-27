01bcc917dcaee70b26d2d26e36c690ea1251d6a5
zicy <kim@zicy.dk>
2024-04-27 03:09:11 +0200

    Code improvements and som minor CSS update

--- js/script.js
+++ js/script.js
@@ -1,125 +1,81 @@
 const csvDataFile = "toeffe_lager.csv?v=15"
 
-// Preload the CSV file
-document.addEventListener('DOMContentLoaded', function () {
 
-    // Pre fetch data
-    localStorage.removeItem('csvData');
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', csvDataFile, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            const csvData = xhr.responseText;
-            localStorage.setItem('csvData', csvData); // Store CSV data in localStorage
-        }
-    };
-    xhr.send();
-
-    // Retrieve stored divs from localStorage
-    var storedPins = localStorage.getItem('savedPins');
-    var storedPinsList = localStorage.getItem('savedPinsList');
-    var storedPinsIds = localStorage.getItem('savedPinsIds');
-
-    // Get references to the divs in the document
-    var savedPins = document.getElementById('map-pins-saved');
-    var savedPinsList = document.getElementById('map-pins-saved-list');
-
-    // Check if both the stored divs and the corresponding divs in the document exist
-    if (storedPins && storedPinsList && savedPins && savedPinsList && storedPinsIds) {
-
-        var storedPinsIds = JSON.parse(storedPinsIds);
-
-        // Replace the content of the divs with the stored content
-        savedPins.innerHTML = storedPins;
-        savedPinsList.innerHTML = storedPinsList;
-
-        // Loop through the array
-        storedPinsIds.forEach(function (marker_id) {
-            // Get the element with the current ID
-            var element = document.getElementById(marker_id);
-
-            // Check if the element exists
-            if (element) {
-                // Add event listener to the element
-                element.addEventListener('click', function () {
-                    removeSavedMarker(marker_id.split('_')[0]); // Clear marker when mouse leaves the result element
-                });
-            } else {
-                console.error("Element with ID", marker_id, "not found.");
-            }
-        });
-    } else {
-        // Handle the case where either the stored divs or the corresponding divs in the document do not exist
-        console.error("Could not find one or more of the required elements.");
-    }
-
-});
 
 function searchCSV(inputValue) {
-
     const csvData = localStorage.getItem('csvData');
-    // Rest of your code to process CSV data...
-    // This part remains unchanged from your original code
-    // You can use the csvData variable here to process the CSV data
+    if (!csvData) {
+        console.error('CSV data not found in localStorage.');
+        return;
+    }
+
     const rows = csvData.split('\n');
-    let count = 0; // Counter for limiting results
     const searchResultsContainer = document.getElementById('search-results');
-    searchResultsContainer.innerHTML = ''; // Clear previous results
+    searchResultsContainer.innerHTML = '';
+
+    let count = 0;
+
     for (let i = 0; i < rows.length; i++) {
         const columns = rows[i].split(',');
-        let random_id = 0;
         if (columns.length >= 2 && columns[1].toLowerCase().includes(inputValue.toLowerCase())) {
-            const newDiv = document.createElement('div');
-            random_id = Math.floor(Math.random() * 1000000)
-
-            const item_name = columns[1];
-            const lunch_box = columns[2];
-            const lunch_box_row = columns[3];
-            const lunch_box_position = columns[4];
-            const image_name = columns[5];
-
-            // Get lunch box information
-            const lunch_box_letter = columns[2].match(/[^\d]+/)[0];
-            const lunch_box_number = parseInt(columns[2].match(/\d+/)[0]);
-
-            var formatted_name = "";
-            if (image_name.length <= 1) {
-                formatted_name = item_name.toLowerCase().replace(/ /g, '_');
-            } else {
-                formatted_name = image_name;
-            }
-
-            newDiv.innerHTML = `<img class="color-${lunch_box_letter}" src="images/block_icons/${formatted_name}.png" onerror="this.onerror=null; this.src='images/MissingTextureBlock.png'" ><div class="result-name">${item_name} </div><div class="result-extra-info"><div>Lunchbox <span class="">${lunch_box}</span></div> <div>Row <span class="">${lunch_box_row}</span></div> <div>Position <span class="">${lunch_box_position}</span></div></div>`;
-            newDiv.classList.add('result', 'pop-in'); // Add classes 'result' and 'pop-in' to each div
-            newDiv.style.animationDelay = `${count * 0.1}s`; // Adjust delay time as needed
-            searchResultsContainer.appendChild(newDiv); // Append new result div
-            // Add marker to map
-            addMarker(item_name, lunch_box_letter, lunch_box_number, lunch_box_row, lunch_box_position, random_id); // Pass latitude and longitude
-
-            // Add event listeners to the dynamically created div
-            newDiv.addEventListener('mouseenter', function () {
-                hideOtherMarkers(random_id); // Call function to hide other markers
-            });
-            newDiv.addEventListener('mouseleave', function () {
-                showOtherMarkers(); // Clear marker when mouse leaves the result element
-            });
-            newDiv.addEventListener('click', function () {
-                saveMarker(lunch_box_letter + lunch_box_number, random_id, item_name); // Clear marker when mouse leaves the result element
-            });
-
-            count++; // Increment counter
+            const random_id = Math.floor(Math.random() * 1000000);
+            const resultDiv = createResultDiv(columns, random_id, count);
+            searchResultsContainer.appendChild(resultDiv);
+            count++;
         }
     }
+
     if (count === 0) {
-        const newDiv = document.createElement('div');
-
-        newDiv.innerHTML = `<img class="" src="images/MissingTextureBlock.png" ><div class="result-name">Not found: ${inputValue} </div><div class="result-extra-info"><div>Lunchbox <span class="">n/a</span></div> <div>Row <span class="">n/a</span></div> <div>Position <span class="">n/a</span></div></div>`;
-        newDiv.classList.add('result', 'pop-in'); // Add classes 'result' and 'pop-in' to each div
-        newDiv.style.animationDelay = `${count * 0.1}s`; // Adjust delay time as needed
-        searchResultsContainer.appendChild(newDiv); // Append new result div
+        const resultDiv = createNotFoundDiv(inputValue);
+        searchResultsContainer.appendChild(resultDiv);
     }
+}
 
+function createResultDiv(columns, random_id, count) {
+    const [row_id, item_name, lunch_box, lunch_box_row, lunch_box_position, image_name] = columns;
+    const lunch_box_letter = lunch_box.match(/[^\d]+/)[0];
+    const lunch_box_number = parseInt(lunch_box.match(/\d+/)[0]);
+    const formatted_name = image_name.length <= 1 ? item_name.toLowerCase().replace(/ /g, '_') : image_name;
+
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+        <img class="color-${lunch_box_letter}" src="images/block_icons/${formatted_name}.png" alt="${item_name}" >
+        <div class="result-name">${item_name}</div>
+        <div class="result-extra-info">
+            <div>Lunchbox <span class="">${lunch_box}</span></div>
+            <div>Row <span class="">${lunch_box_row}</span></div>
+            <div>Position <span class="">${lunch_box_position}</span></div>
+        </div>
+    `;
+    newDiv.classList.add('result', 'pop-in');
+    newDiv.style.animationDelay = `${count * 0.1}s`;
+
+    // Add event listeners
+    newDiv.addEventListener('mouseenter', () => hideOtherMarkers(random_id));
+    newDiv.addEventListener('mouseleave', showOtherMarkers);
+    newDiv.addEventListener('click', () => saveMarker(`${lunch_box_letter}${lunch_box_number}`, random_id, item_name));
+
+    // Add map marker
+    addMarker(item_name, lunch_box_letter, lunch_box_number, lunch_box_row, lunch_box_position, random_id); // Pass latitude and longitude
+
+    return newDiv;
+}
+
+function createNotFoundDiv(inputValue) {
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+        <img class="" src="images/MissingTextureBlock.png" >
+        <div class="result-name">Not found: ${inputValue}</div>
+        <div class="result-extra-info">
+            <div>Lunchbox <span class="">n/a</span></div>
+            <div>Row <span class="">n/a</span></div>
+            <div>Position <span class="">n/a</span></div>
+        </div>
+    `;
+    newDiv.classList.add('result', 'pop-in');
+    newDiv.style.animationDelay = '0s'; // No delay for not found items
+
+    return newDiv;
 }
 
 
@@ -153,85 +109,69 @@ function clearMarker() {
 }
 
 function saveMarker(lunch_box, marker_id, marker_name) {
+    var storedPinsIds = JSON.parse(localStorage.getItem('savedPinsIds')) || [];
 
-    var storedPinsIds = localStorage.getItem('savedPinsIds');
-    if (storedPinsIds) {
-        var storedPinsIds = JSON.parse(storedPinsIds);
-    } else {
-        var storedPinsIds = [];
+    var originalParent = document.getElementById(lunch_box);
+    if (!originalParent) {
+        console.error("The original parent with ID '" + lunch_box + "' doesn't exist.");
+        return;
     }
 
-    // Get the parent element of the target div
-    var originalParent = document.getElementById(lunch_box);
-
-    // Array of suffixes for the elements to clone
     var suffixes = ['_marker', '_pulse', '_text'];
-
-    // Initialize variables
     var originalElems = [];
     var copiedElems = [];
 
-    // Loop through each suffix to get original elements and clone them
     suffixes.forEach(function (suffix) {
         var originalElem = document.getElementById(marker_id + suffix);
         if (originalElem) {
             originalElems.push(originalElem);
             var clonedElem = originalElem.cloneNode(true);
             if (suffix === '_marker') {
-                // Add class to the cloned element if the suffix is "_marker"
                 clonedElem.classList.add('pin-saved');
             } else if (suffix === '_pulse') {
                 clonedElem.classList.add('pulse-saved');
             }
-
             clonedElem.classList.add('pin-pointer');
             clonedElem.addEventListener('click', function () {
-                removeSavedMarker(marker_id); // Clear marker when mouse leaves the result element
+                removeSavedMarker(marker_id);
             });
             copiedElems.push(clonedElem);
         }
     });
 
-    // Check if any original element exists
     if (originalElems.length > 0) {
-        // Make a copy of the parent div
-        var copiedParent = originalParent.cloneNode(false); // Shallow copy without children
+        var copiedParent = originalParent.cloneNode(false);
         copiedParent.id = copiedParent.id + '_saved';
 
-        // Append the copied child divs to the copied parent div
         copiedElems.forEach(function (copiedElem) {
             copiedParent.appendChild(copiedElem);
         });
 
-        // Optionally, modify the copied parent div or its attributes
-        // copiedParent.id = copiedParent.id + '-saved';
-
-        // Append the copied parent div wherever you want in the DOM
         var savedPinContainer = document.getElementById('map-pins-saved');
-        savedPinContainer.appendChild(copiedParent);
+        if (savedPinContainer) {
+            savedPinContainer.appendChild(copiedParent);
+        } else {
+            console.error("The div with ID 'map-pins-saved' doesn't exist.");
+            return;
+        }
 
-        // Create a new paragraph element
         var paragraph = document.createElement('p');
         paragraph.id = marker_id + '_saved-list';
-
-        // Set the inner HTML of the paragraph
         paragraph.innerHTML = marker_name + '<span>×</span>';
 
-        // Get the div with the ID "map-pins-saved-list"
         var savedListDiv = document.getElementById('map-pins-saved-list');
-
-        // Check if the div exists
         if (savedListDiv) {
-            // Append the paragraph to the div
             savedListDiv.appendChild(paragraph);
             paragraph.addEventListener('click', function () {
-                removeSavedMarker(marker_id); // Clear marker when mouse leaves the result element
+                removeSavedMarker(marker_id);
             });
         } else {
             console.error("The div with ID 'map-pins-saved-list' doesn't exist.");
+            return;
         }
     } else {
         console.error("The original parent or the original div doesn't exist.");
+        return;
     }
 
     suffixes.push('_saved-list');
@@ -245,14 +185,12 @@ function saveMarker(lunch_box, marker_id, marker_name) {
 }
 
 function removeSavedMarker(marker_id) {
-
-    // Array of suffixes for the elements to clone
     var suffixes = ['_marker', '_pulse', '_text', '_saved-list'];
 
     suffixes.forEach(function (suffix) {
-        var originalElem = document.getElementById(marker_id + suffix);
-        if (originalElem) {
-            originalElem.remove();
+        var element = document.getElementById(marker_id + suffix);
+        if (element) {
+            element.remove();
         }
     });
 
@@ -260,40 +198,22 @@ function removeSavedMarker(marker_id) {
 }
 
 function updateSavedMarkerStorage() {
-
     var savedPins = document.getElementById('map-pins-saved');
     var savedPinsList = document.getElementById('map-pins-saved-list');
-    var storedPinsIds = localStorage.getItem('savedPinsIds');
+    var storedPinsIds = JSON.parse(localStorage.getItem('savedPinsIds')) || [];
 
-    if (storedPinsIds) {
-        var storedPinsIds = JSON.parse(storedPinsIds);
-    } else {
-        var storedPinsIds = [];
-    }
-
-    if (savedPins && savedPinsList && storedPinsIds) {
-
-        // Create a new array to store the IDs of divs that exist
-        var pinsIds = [];
-
-        // Loop through the array of IDs
-        storedPinsIds.forEach(function(id) {
-            // Get the element with the current ID
-            var element = document.getElementById(id);
-            
-            // If the element exists, add the ID to the new array
-            if (element) {
-                pinsIds.push(id);
-            }
+    if (savedPins && savedPinsList && storedPinsIds.length > 0) {
+        var pinsIds = storedPinsIds.filter(function (id) {
+            return document.getElementById(id);
         });
 
-        localStorage.setItem('savedPins', savedPins.innerHTML); // Store CSV data in localStorage
-        localStorage.setItem('savedPinsList', savedPinsList.innerHTML); // Store CSV data in localStorage
+        localStorage.setItem('savedPins', savedPins.innerHTML);
+        localStorage.setItem('savedPinsList', savedPinsList.innerHTML);
         localStorage.setItem('savedPinsIds', JSON.stringify(pinsIds));
-
-
     }
 }
+
+
 
 // L1 = lunchbox
 // Tæller fra middergang 
@@ -377,7 +297,78 @@ function addMarker(name, lunch_box_letter, lunch_box_number, level, position, ra
     mapContainer.appendChild(markerText);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+function clear_input(event2) {
+    const inputBox = document.getElementById('search-input-field');
+    var event = new KeyboardEvent('keyup', {
+        key: 'Backspace',           // Specify the key you want to simulate
+        code: 'Backspace',       // Specify the code of the key (optional)
+        keyCode: 83,        // Specify the key code (optional)
+        which: 83,          // Specify the key code (optional)
+        bubbles: true       // Allow the event to bubble up (optional)
+    });
+    inputBox.value = '';
+    inputBox.dispatchEvent(event);
+}
+
+// Preload the CSV file
+document.addEventListener('DOMContentLoaded', async function () {
+    // Check for localStorage support
+    if (!localStorage) {
+        console.error('LocalStorage is not supported in this browser.');
+        return;
+    }
+
+
+    // Pre-fetch data
+    localStorage.removeItem('csvData');
+    try {
+        const response = await fetch(csvDataFile);
+        if (!response.ok) {
+            throw new Error('Failed to fetch CSV data');
+        }
+        const csvData = await response.text();
+        localStorage.setItem('csvData', csvData);
+    } catch (error) {
+        console.error('Error fetching CSV data:', error);
+    }
+
+    // Restore saved pins
+
+    // Retrieve stored divs from localStorage
+    const storedPins = localStorage.getItem('savedPins');
+    const storedPinsList = localStorage.getItem('savedPinsList');
+    const storedPinsIds = localStorage.getItem('savedPinsIds');
+
+    // Get references to the divs in the document
+    const savedPins = document.getElementById('map-pins-saved');
+    const savedPinsList = document.getElementById('map-pins-saved-list');
+
+    // Check if both the stored divs and the corresponding divs in the document exist
+    if (storedPins && storedPinsList && savedPins && savedPinsList && storedPinsIds) {
+        try {
+            const storedPinsIdsArray = JSON.parse(storedPinsIds);
+
+            savedPins.innerHTML = storedPins;
+            savedPinsList.innerHTML = storedPinsList;
+
+            storedPinsIdsArray.forEach(markerId => {
+                const element = document.getElementById(markerId);
+                if (element) {
+                    element.addEventListener('click', () => {
+                        removeSavedMarker(markerId.split('_')[0]);
+                    });
+                } else {
+                    console.error('Element with ID', markerId, 'not found.');
+                }
+            });
+        } catch (error) {
+            console.error('Error parsing storedPinsIds:', error);
+        }
+    } else {
+        console.error('Could not find one or more of the required elements.');
+    }
+
+    // Setup search box
     const inputBox = document.getElementById('search-input-field');
     const inputBoxIcon = document.getElementById('search-input-icon');
     const searchResultsContainer = document.getElementById('search-results');
@@ -400,17 +391,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
-function clear_input(event2) {
-    const inputBox = document.getElementById('search-input-field');
-    var event = new KeyboardEvent('keyup', {
-        key: 'Backspace',           // Specify the key you want to simulate
-        code: 'Backspace',       // Specify the code of the key (optional)
-        keyCode: 83,        // Specify the key code (optional)
-        which: 83,          // Specify the key code (optional)
-        bubbles: true       // Allow the event to bubble up (optional)
-    });
-    inputBox.value = '';
-    inputBox.dispatchEvent(event);
-}
-
